45aba4f7d71f947dd214173ff23cbb396c5206c0
Manuel-latorre <manuel.latorre11@gmail.com>
2023-12-13 17:09:15 -0300

    add chanhes in postToken

--- controller/getToken.js
+++ controller/getToken.js
@@ -1,15 +1,23 @@
 const tokenSchema = require('../models/token');
 
 const getToken = async (req, res) => {
-    try {
-        
-        const tokens = await tokenSchema.find({}, 'token');
-        res.status(200).json({ tokens });
-        console.log(tokens);
-      } catch (error) {
-        console.error('Error al obtener tokens de la base de datos:', error);
-        res.status(500).json({ error: 'Error al obtener tokens' });
-      }
-}
+  try {
+      const tokens = await tokenSchema.find({});
+      const totalTokens = tokens.length;
+
+      res.status(200).json({
+          success: true,
+          message: 'Tokens recuperados con éxito',
+          totalTokens,
+          tokens,
+      });
+
+      console.log(tokens);
+  } catch (error) {
+      console.error('Error al obtener tokens de la base de datos:', error);
+      res.status(500).json({ success: false, error: 'Error al obtener tokens' });
+  }
+};
+
 
 module.exports = getToken; 
--- controller/postToken.js
+++ controller/postToken.js
@@ -4,13 +4,20 @@ const postToken = async (req, res) => {
   const { fcmtoken } = req.body;
 
   try {
-    // Lógica para guardar el token en la base de datos
-    const newToken = await tokenSchema.create({ token: fcmtoken });
-    console.log('Token registrado en la base de datos:', newToken);
-    res.status(200).json({ message: 'Token registrado con éxito' });
+    // Verifica si el token ya está registrado para evitar duplicados
+    const existingToken = await tokenSchema.findOne({ token: fcmtoken });
+
+    if (!existingToken) {
+      // Si el token no existe, guárdalo en la base de datos
+      await Token.create({ token: fcmtoken });
+      res.status(200).json({ message: 'Token registrado con éxito' });
+    } else {
+      // Si el token ya existe, responde indicando que ya está registrado
+      res.status(200).json({ message: 'El token ya está registrado' });
+    }
   } catch (error) {
     console.error('Error al registrar el token en la base de datos:', error);
-    res.status(500).json({ error: 'Error al registrar el token' });
+    res.status(500).json({ error: 'Error al registrar el token en la base de datos' });
   }
 }
 
